68e9d97be0677c113b779c820d08113a048b7bfb
guoci <zguoci@gmail.com>
2019-06-28 15:46:01 -0400

    new version

--- DIA-Umpire/src/MSUmpire/BaseDataStructure/UmpireInfo.java
+++ DIA-Umpire/src/MSUmpire/BaseDataStructure/UmpireInfo.java
@@ -26,7 +26,7 @@ package MSUmpire.BaseDataStructure;
 public class UmpireInfo {
 
     private static UmpireInfo umpireInfo = null;
-    public String Version = "v2.1.5";
+    public String Version = "v2.1.6";
 
     private UmpireInfo() {
 
