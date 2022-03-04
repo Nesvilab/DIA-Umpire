66543f41988ebc76c0ec5ba9705e92825b6df9df
guoci <zguoci@gmail.com>
2022-03-04 14:37:18 -0500

    bump version

--- DIA-Umpire/src/MSUmpire/BaseDataStructure/UmpireInfo.java
+++ DIA-Umpire/src/MSUmpire/BaseDataStructure/UmpireInfo.java
@@ -26,7 +26,7 @@ package MSUmpire.BaseDataStructure;
 public class UmpireInfo {
 
     private static UmpireInfo umpireInfo = null;
-    public String Version = "v2.2.6";
+    public String Version = "v2.2.7";
 
     private UmpireInfo() {
 
