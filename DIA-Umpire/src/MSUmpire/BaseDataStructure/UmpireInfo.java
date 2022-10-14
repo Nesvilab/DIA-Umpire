e932bdaf81caa61c1579392732fe5671ef4401b0
Fengchao <fcyucn@gmail.com>
2022-10-14 14:35:55 -0400

    Bump to 2.3.0.

--- DIA-Umpire/src/MSUmpire/BaseDataStructure/UmpireInfo.java
+++ DIA-Umpire/src/MSUmpire/BaseDataStructure/UmpireInfo.java
@@ -26,7 +26,7 @@ package MSUmpire.BaseDataStructure;
 public class UmpireInfo {
 
     private static UmpireInfo umpireInfo = null;
-    public String Version = "v2.2.9";
+    public String Version = "v2.3.0";
 
     private UmpireInfo() {
 
